b19dae7dba8b3dab1175b475b35e9eef72216ffa
ohad <ohad@theora.com>
2020-09-06 12:51:57 +0300

    SnapShot

--- src/index.php
+++ src/index.php
@@ -4,6 +4,17 @@ require_once("trackingConfig.php");
 require_once(M_DIR."/mfiles.php");
 require_once("trackingFiles.php");
 /*------------------------------------------------------------*/
+$ua = @$_SERVER['HTTP_USER_AGENT'];
+if (
+	! $ua
+	|| stristr($ua, "bot")
+	|| stristr($ua, "crawl")
+	|| stristr($ua, "spider")
+	) {
+	http_response_code(204);
+	exit;
+}
+/*------------------------------------------------------------*/
 global $Mview;
 global $Mmodel;
 $Mview = new Mview;
